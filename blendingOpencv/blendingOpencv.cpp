8cae49b142e909333cd388a82c1a6a396bc37def
uedatatsu <uedatatsu02@gmail.com>
2019-05-02 11:40:44 +0900

    2019-05-02

--- blendingOpencv/blendingOpencv.cpp
+++ blendingOpencv/blendingOpencv.cpp
@@ -14,8 +14,9 @@ int main()
 {
 	//画像生成
 	cv::Mat img(cv::Size(1024, 768), CV_8UC1, cv::Scalar(0));	
-	cv::circle(img, cv::Point(400, 300), 50, cv::Scalar(50), -1, 8, 0);
-	cv::circle(img, cv::Point(800, 400), 50, cv::Scalar(199), -1, 8, 0);
+	img = cv::imread("./images/0.png", cv::IMREAD_GRAYSCALE);
+	//cv::circle(img, cv::Point(400, 300), 50, cv::Scalar(50), -1, 8, 0);
+	//cv::circle(img, cv::Point(800, 400), 50, cv::Scalar(199), -1, 8, 0);
 	//cv::threshold(img,                // 入力配列（シングルチャンネル，8ビット，あるいは32ビット浮動小数点型）．
 	//	img,
 	//	200,                // 閾値．
@@ -23,9 +24,15 @@ int main()
 	//	cv::THRESH_BINARY_INV   // 閾値処理の種類.
 	//);
 	cv::distanceTransform(img, img, cv::DIST_L2,5);
-	
-	cv::imshow("window", img/20);
-	cv::waitKey(100000);
+	//img = img / 25;
+
+	//std::cout << img << std::endl;
+	//cv::imshow("window", img);
+	cv::waitKey(100);
+	cv::imwrite("out.png", img*5 );
+
+	cv::waitKey(1);
+
 	//getchar();
 }
 
